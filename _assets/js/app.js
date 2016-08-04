ef49e3396befee224442b06b9d0e775cef97dd99
James Shakespeare <j@jshakespeare.com>
2016-08-03 22:08:59 -0400

    show phase name

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -12,6 +12,7 @@ if ('geolocation' in navigator) {
       const phase = data.daily.data[0].moonPhase * 100;
       const hemisphere = position.coords.latitude > 0 ? 'north' : 'south';
       setMoonClass(phase, hemisphere);
+      document.getElementById('phase-name').innerHTML = getPhaseName(phase);
     });
   });
 }
@@ -19,6 +20,27 @@ if ('geolocation' in navigator) {
 function setMoonClass(phase, hemisphere) {
   let moonEl = document.getElementById('moon');
   moonEl.setAttribute('class', `phase-${phase} ${hemisphere}ern-hemisphere`);
+}
+
+function getPhaseName(phase) {
+  switch(true) {
+    case (phase < 12.5):
+      return 'New moon';
+    case (phase < 25):
+      return 'Waxing crescent';
+    case (phase < 37.5):
+      return 'First quarter';
+    case (phase < 50):
+      return 'Waxing gibbous';
+    case (phase < 62.5):
+      return 'Full moon';
+    case (phase < 75):
+      return 'Waning gibbous';
+    case (phase < 87.5):
+      return 'Last quarter';
+    case (phase < 100):
+      return 'Waning crescent';
+  }
 }
 
 function getMoonPhase(latlong) {
